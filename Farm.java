34c302765dbfe49c68e12ab05efcf71c65bac8a2
BTLSandwich <btl369x3@gmail.com>
2018-10-22 14:06:16 -0700

    stuck in a rock and a hard place

--- Farm.java
+++ Farm.java
@@ -1,4 +1,18 @@
 class Farm 
 {     
-   //your code here
+   private Animal[] BunchOfAnimals;
+   public Farm()
+   {
+      BunchOfAnimals = new Animal[3];
+      BunchOfAnimals[0] = new Cow("cow","moo");
+      BunchOfAnimals[1] = new Chick("chick","cluck");
+      BunchOfAnimals[2] = new Pig("pig","oink");
+   }
+   public void animalSounds()
+   {
+      for (int nI=0; nI < BunchOfAnimals.length; nI++)
+      {
+         System.out.println(BunchOfAnimals[nI].getType() + " goes " + BunchOfAnimals[nI].getSound()); 
+      }
+   }
 }
