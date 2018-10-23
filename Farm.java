c83430ae72bce5444631fc2da7831c8d48649dfc
BTLSandwich <btl369x3@gmail.com>
2018-10-23 13:36:52 -0700

    fin

--- Farm.java
+++ Farm.java
@@ -4,15 +4,16 @@ class Farm
    public Farm()
    {
       BunchOfAnimals = new Animal[3];
-      BunchOfAnimals[0] = new Cow("cow","moo");
-      BunchOfAnimals[1] = new Chick("chick","cluck");
+      BunchOfAnimals[0] = new NamedCow("cow","Vladimir the Third of the Holy Russian Empire","moo");
+      BunchOfAnimals[1] = new Chick("chick","cheep","cluck");
       BunchOfAnimals[2] = new Pig("pig","oink");
    }
    public void animalSounds()
    {
       for (int nI=0; nI < BunchOfAnimals.length; nI++)
       {
-         System.out.println(BunchOfAnimals[nI].getType() + " goes " + BunchOfAnimals[nI].getSound()); 
+         System.out.println("The " + BunchOfAnimals[nI].getType() + " goes " + BunchOfAnimals[nI].getSound()); 
       }
+      System.out.println("The cow is known as " + ((NamedCow)BunchOfAnimals[0]).getName());
    }
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,5 +1,6 @@
 
 public void setup()
 {
-	
+	Farm Gundham = new Farm();
+  Gundham.animalSounds();
 }
